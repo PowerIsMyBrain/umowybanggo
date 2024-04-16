459fbbabc485c4eb88ddc2edfd5698e7f248f4b2
PowerIsMyBrain <powerimbrain@gmail.com>
2024-04-16 22:25:34 +0200

    umowy_django

--- umowyFLA/run.py
+++ umowyFLA/run.py
@@ -1,33 +1,21 @@
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
+
 import speech_recognition as sr
 from flask import jsonify
-# from pydub import AudioSegment
+
 import soundfile as sf
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('socketdata.html')
 
-@socketio.on('voice_data')
-def handle_voice_data(data):
-    print(data)  # Wyświetlenie danych w konsoli serwera
-    # Tutaj możesz przetworzyć otrzymane dane, np. zapisać je do pliku, przekazać do innej funkcji itp.
-    emit('voice_response', {'status': 'ok'})  # Wysłanie odpowiedzi do klienta
-
 @app.route('/voice')
 def voice():
     return render_template("base_app_one.html")
 
-# @app.route('/send_recording', methods=['POST'])  # Nowy endpoint do przechwytywania danych z przycisku "Wyślij nagranie"
-# def send_recording():
-#     # Tutaj możesz przechwycić dane wysłane z przeglądarki i przetworzyć je, np. zapisać do pliku
-#     data = request.data.decode('utf-8')  # Pobranie danych przesłanych w żądaniu
-#     print("Received recording:", data)  # Wyświetlenie danych w konsoli serwera
-#     return "Recording received successfully"
+
 
 
 
@@ -54,20 +42,33 @@ def send_recording():
 
     with sr.AudioFile('received_recording.wav') as source:
         audio = recognizer.record(source)
-    
+    found = False
     try:
-        text = recognizer.recognize_google(audio, language='pl-PL')
-        recognized_texts['format'] = text
+        rcog = recognizer.recognize_google(audio, language='pl-PL')
+        # Tutaj serce programu 
+        text = str(rcog).lower().replace(' kropka', '.')\
+            .replace(' kropka ', '. ').replace(' przecinek ', ', ')\
+            .replace(' wykrzyknik ', '! ').replace(' wykrzyknik', '!')\
+            .replace(' znak zapytania ', '? ').replace(' znak zapytania', '?')
+            
+        if text.count('umowa o dzielo') > 0:
+            found = True
+            text = text + """
+
+            
+            Znaleziono umowę o dzieło!
+            """
+        
         print('text ok')
     except sr.UnknownValueError:
-        recognized_texts['format'] = "Nie można rozpoznać mowy"
-        print(recognized_texts['format'])
+        text = "Nie można rozpoznać mowy"
+
     except sr.RequestError as e:
-        recognized_texts['format'] = f"Błąd serwera: {e}"
-    print(recognized_texts['format'])
+        text = f"Błąd serwera: {e}"
+
     # Przekazanie wyników rozpoznawania mowy do strony internetowej za pomocą JSON
-    return jsonify({'recognized_texts': recognized_texts})
+    return jsonify({'recognized_texts': text})
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=8000)
+    app.run(debug=True, port=8000)
